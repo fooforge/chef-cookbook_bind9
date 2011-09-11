5bb8b4da8c6f242490acb7636ab457bcdd9f76e7
Mike Adolphs <mike@fooforge.com>
2011-09-11 14:33:45 +0200

    Initial commit, empty cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Mike Adolphs"
+maintainer_email "mike@fooforge.com"
+license          "Apache 2.0"
+description      "Installs/Configures bind9"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
