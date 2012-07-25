ba43478219979c834314d12115d74ce620c731b5
Mike Adolphs <mike@fooforge.com>
2012-07-25 13:30:08 +0200

    Several corrections to the default recipe, fixed typo. (I know, this cookbooks still needs a lot of work to be useful at all. Apologies.)

--- recipes/default.rb
+++ recipes/default.rb
@@ -73,24 +73,12 @@ search(:zones).each do |zone|
 	end
 
 	template "#{node[:bind9][:config_path]}/#{zone['domain']}" do
-		source "#{node[:bind9][:config_path]}/#{zone['domain']}.erb"
-		local true
-		owner "root"
-		group "root"
-		mode 0644
-		notifies :restart, resources(:service => "bind9")
-		variables({
-			:serial => Time.new.strftime("%Y%m%d%H%M%S")
-		})
-		action :nothing
-	end
-
-	template "#{node[:bind9][:config_path]}/#{zone['domain']}.erb" do
 		source "zonefile.erb"
 		owner "root"
 		group "root"
 		mode 0644
 		variables({
+			:serial => Time.new.strftime("%Y%m%d%H%M%S"),
 			:domain => zone['domain'],
 			:soa => zone['zone_info']['soa'],
 			:contact => zone['zone_info']['contact'],
@@ -99,7 +87,7 @@ search(:zones).each do |zone|
 			:mail_exchange => zone['zone_info']['mail_exchange'],
 			:records => zone['zone_info']['records']
 		})
-		notifies :create, resources(:template => "#{node[:bind9][:config_path]}/#{zone['domain']}"), :immediately
+		notifies :restart, resources(:service => "bind9")
 	end
 end
 
