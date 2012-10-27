75996198129dccf9f9a00b48ad49729dab6c32d3
Mike Adolphs <mike@fooforge.com>
2012-10-27 18:40:33 +0200

    Make sure that the serial doesn't get incremented on every chef-run

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,11 +18,11 @@
 #
 
 package "bind9" do
-	case node[:platform]
-	when "centos", "redhat", "suse", "fedora"
-		package_name "bind"
-	end
-	action :install
+  case node[:platform]
+  when "centos", "redhat", "suse", "fedora"
+    package_name "bind"
+  end
+  action :install
 end
 
 directory "/var/log/bind/" do
@@ -32,65 +32,77 @@ directory "/var/log/bind/" do
 end
 
 service "bind9" do
-	case node[:platform]
-	when "centos", "redhat"
-		service_name "named"
-	end
-	supports :status => true, :reload => true, :restart => true
-	action [ :enable ]
+  case node[:platform]
+  when "centos", "redhat"
+    service_name "named"
+  end
+  supports :status => true, :reload => true, :restart => true
+  action [ :enable ]
 end
 
 template node[:bind9][:options_file] do
-	source "named.conf.options.erb"
-	owner "root"
-	group "root"
-	mode 0644
+  source "named.conf.options.erb"
+  owner "root"
+  group "root"
+  mode 0644
   notifies :restart, resources(:service => "bind9")
 end
 
 template node[:bind9][:local_file] do
-	source "named.conf.local.erb"
-	owner "root"
-	group "root"
-	mode 0644
-	variables({
-		:zonefiles => search(:zones)
-	})
+  source "named.conf.local.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables({
+    :zonefiles => search(:zones)
+  })
   notifies :restart, resources(:service => "bind9")
 end
 
 
 search(:zones).each do |zone|
-	unless zone['autodomain'].nil? || zone['autodomain'] == ''
-		search(:node, "domain:#{zone['autodomain']}").each do |host|
-			next if host['ipaddress'] == '' || host['ipaddress'].nil?
-			zone['zone_info']['records'].push( {
-				"name" => host['hostname'],
-				"type" => "A",
-				"ip" => host['ipaddress']
-			})
-		end
-	end
+  unless zone['autodomain'].nil? || zone['autodomain'] == ''
+    search(:node, "domain:#{zone['autodomain']}").each do |host|
+      next if host['ipaddress'] == '' || host['ipaddress'].nil?
+      zone['zone_info']['records'].push( {
+        "name" => host['hostname'],
+        "type" => "A",
+        "ip" => host['ipaddress']
+      })
+    end
+  end
 
-	template "#{node[:bind9][:config_path]}/#{zone['domain']}" do
-		source "zonefile.erb"
-		owner "root"
-		group "root"
-		mode 0644
-		variables({
-			:serial => Time.new.strftime("%Y%m%d%H%M%S"),
-			:domain => zone['domain'],
-			:soa => zone['zone_info']['soa'],
-			:contact => zone['zone_info']['contact'],
-			:global_ttl => zone['zone_info']['global_ttl'],
-			:nameserver => zone['zone_info']['nameserver'],
-			:mail_exchange => zone['zone_info']['mail_exchange'],
-			:records => zone['zone_info']['records']
-		})
-		notifies :restart, resources(:service => "bind9")
-	end
+  template "#{node[:bind9][:config_path]}/#{zone['domain']}" do
+    source "#{node[:bind9][:config_path]}/#{zone['domain']}.erb"
+    local true
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :restart, resources(:service => "bind9")
+    variables({
+      :serial => Time.new.strftime("%Y%m%d%H%M%S")
+    })
+    action :nothing
+  end
+
+  template "#{node[:bind9][:config_path]}/#{zone['domain']}.erb" do
+    source "zonefile.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables({
+      :domain => zone['domain'],
+      :soa => zone['zone_info']['soa'],
+      :contact => zone['zone_info']['contact'],
+      :global_ttl => zone['zone_info']['global_ttl'],
+      :nameserver => zone['zone_info']['nameserver'],
+      :mail_exchange => zone['zone_info']['mail_exchange'],
+      :records => zone['zone_info']['records']
+    })
+    notifies :create, resources(:template => "#{node[:bind9][:config_path]}/#{zone['domain']}"), :immediately
+  end
 end
 
 service "bind9" do
-	action [ :start ]
+  action [ :start ]
 end
